5f993bdeb3917882f08f3fe1b9b3641be497ebad
cherryCRazY <bebebe12@ukr.net>
2019-02-19 08:50:13 +0200

    connect Layout component to redux

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,16 +1,56 @@
+//Core
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+//Components
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+import Spinner from "../../Components/UI/Spinner";
+import modal from "../../Components/UI/Modal";
+import Posts from "../Posts";
 
 //Styles
 import Styles from "./styles.module.scss";
 
-class index extends Component {
+import blogAction from "../../bus/blog/actions";
+
+class Layout extends Component {
+    componentDidMount() {
+        this.props.getListOfPosts();
+    }
     render() {
+        const { isFetching, errorLabel, errorMessage, posts } = this.props;
         return (
-            <main className={Styles.Layout}>
-                <header className={Styles.header} />
-            </main>
+            <>
+                {errorLabel ? modal("error", errorMessage) : null}
+
+                {isFetching ? (
+                    <Spinner />
+                ) : (
+                    <main className={Styles.Layout}>
+                        <Header className={Styles.header} />
+                        <div>
+                            <Posts data={posts} />
+                        </div>
+                        <Footer />
+                    </main>
+                )}
+            </>
         );
     }
 }
+const mapStateToProps = state => ({
+    isFetching: state.ui.get("isFetching"),
+    errorLabel: state.ui.get("errorLabel"),
+    errorMessage: state.ui.get("errorMessage"),
+    posts: state.blog.get("posts").toJS()
+});
 
-export default index;
+const mapDispatchToProps = dispatch => ({
+    getListOfPosts: () => dispatch(blogAction.getListOfPostsAsync())
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Layout);
---
+++ src/containers/Posts/index.js
@@ -0,0 +1,27 @@
+//Core
+import React, { Component } from "react";
+
+//Components
+import PostCard from "../../Components/PostCard";
+//Styles
+import Styles from "./styles.module.scss";
+
+class Posts extends Component {
+    render() {
+        console.log(this.props.data);
+        const posts = this.props.data.map(p => (
+            <PostCard
+                key={p.id}
+                id={p.id}
+                title={p.title}
+                body={p.body}
+                author={p.author}
+                date={p.date}
+            />
+        ));
+
+        return <div>{posts}</div>;
+    }
+}
+
+export default Posts;
