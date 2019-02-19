a31b2925e1c86fabebbf8dd10aef7937638956bc
cherryCRazY <bebebe12@ukr.net>
2019-02-19 17:56:04 +0200

    small saga tests

--- src/REST/tests/addComment.test.js
+++ src/REST/tests/addComment.test.js
@@ -1,34 +1,36 @@
 //Core
 import { put, apply } from "redux-saga/effects";
-// import { cloneableGenerator } from "redux-saga/utils";
 
 //Instruments
 import blogActions from "../../bus/blog/actions";
 import uiActions from "../../bus/ui/actions";
 import { addComment } from "../../bus/blog/saga/workers";
+import { Api } from "../api";
 
-const addCommentAsync = blogActions.addCommentAsync();
-const postObj = {
+const testObj = {
     comment: {
-        title: "Test title",
+        postId: "Test postId",
         body: "Test body"
     }
 };
 
-const saga = addComment({}); //cloneableGenerator(addComment)(addCommentAsync);
-let clone = null;
+const saga = addComment(testObj);
 
 //Cases
-describe("fillmarketItems saga: ", () => {
+describe("addComment saga: ", () => {
     describe("should pass till response received", () => {
         test("should dispatch startFetching action", () => {
             expect(saga.next().value).toEqual(put(uiActions.startFetching()));
         });
-        // test("should call a fetch request", () => {
-        //     expect(saga.next().value).toEqual(
-        //         apply(Api, Api.market.fetchMarketItems)
-        //     );
-        //     clone = saga.clone();
-        // });
+        test("should call a fetch request", () => {
+            expect(saga.next().value).toEqual(
+                apply(Api, Api.addComment, [testObj.comment])
+            );
+        });
+        test("should dispatch a receivePostAsync action", () => {
+            expect(saga.next().value).toEqual(
+                put(blogActions.receivePostAsync(testObj.comment.postId))
+            );
+        });
     });
 });
