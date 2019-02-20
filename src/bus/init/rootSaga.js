ac7cbcb61dd8e781cb53addc0b038579be556f02
cherryCRazY <bebebe12@ukr.net>
2019-02-21 01:14:22 +0200

    rename saga wather

--- src/bus/init/rootSaga.js
+++ src/bus/init/rootSaga.js
@@ -1,6 +1,6 @@
-import logger from "../blog/saga/watcher";
+import blog from "../blog/saga/watcher";
 import { call, all } from "redux-saga/effects";
 
 export default function* rootSaga() {
-    yield all([call(logger)]);
+    yield all([call(blog)]);
 }
